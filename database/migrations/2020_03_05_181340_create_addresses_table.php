ec6801be3227b046aa18634969db2356c2c90475
gabcostab4 <gabriel.silva@before.com.br>
2020-03-05 14:20:18 -0400

    feat: add migrations

---
+++ database/migrations/2020_03_05_181340_create_addresses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blue23print $table) {
+            $table->id();
+            $table->string('zip_code')->unique();
+            $table->string('street');
+            $table->string('state');
+            $table->string('country');
+            $table->string('city');
+            $table->string('neighborhood')->nullable();
+            $table->unsignedBigInteger('owner');
+
+            $table->foreign('owner')->references('id')->on('people');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
